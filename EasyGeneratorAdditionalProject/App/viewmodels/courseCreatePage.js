6c3e6e2c1b35a89d623ec29df33b72b03b5ac623
Aleksander Bulakh <llutor.2013@gmail.com>
2016-07-21 23:01:42 +0300

    client validation

--- EasyGeneratorAdditionalProject/App/viewmodels/courseCreatePage.js
+++ EasyGeneratorAdditionalProject/App/viewmodels/courseCreatePage.js
@@ -1,8 +1,32 @@
 ﻿define(['knockout', 'plugins/router', 'durandal/app', 'data/courseRepository'],
     function (ko, router, app, courseRepository) {
+
+        ko.extenders.validName = function (target, overrideMessage) {
+            target.hasError = ko.observable();
+            target.validationMessage = ko.observable();
+
+            function validate(newValue) {
+                var check;
+                if (!newValue)
+                    check = false;
+                else {
+                    newValue = newValue.trim();
+                    check = (newValue.length <= 255 && newValue.length > 0);
+                }
+                target.hasError(!check);
+                target.validationMessage(check ? "" : overrideMessage || "This field is required");
+            }
+
+            validate(target());
+            target.subscribe(validate);
+            return target;
+        };
+
         return {
             courseId: ko.observable(),
-            courseTitle: ko.observable(),
+            courseTitle: ko.observable().extend({
+                validName: 'Please, enter course title! Maximum number of characters - 255.'
+            }),
             courseSection: ko.observable(),
             courseDescription: ko.observable(),
             activate: function (id) {

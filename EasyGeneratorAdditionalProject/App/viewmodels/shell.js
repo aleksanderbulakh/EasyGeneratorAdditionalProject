b759f83ba7e7d91b1a93db24700345362ce7cb67
Aleksander Bulakh <llutor.2013@gmail.com>
2016-08-02 10:05:43 +0300

    demo 1.0.0.1

--- EasyGeneratorAdditionalProject/App/viewmodels/shell.js
+++ EasyGeneratorAdditionalProject/App/viewmodels/shell.js
@@ -8,8 +8,8 @@
 
             router.map([
                 { route: '', title: 'Course list', moduleId: 'viewmodels/courseList', nav: true },
-                { route: 'course(/:id)', title: 'Course', moduleId: 'viewmodels/course', nav: true },
-                { route: 'preview(/:id)', title: 'Prewiew', moduleId: 'viewmodels/previewCourse', nav: true }
+                { route: 'course/:id', title: 'Course', moduleId: 'viewmodels/course', nav: true },
+                { route: 'preview/:id', title: 'Prewiew', moduleId: 'viewmodels/previewCourse', nav: true }
             ]).buildNavigationModel();
 
             return router.activate();
